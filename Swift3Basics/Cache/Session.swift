cdaf4b78846682b3cf83a7607cf0ea10ac185201
hw <hw3308@sina.com>
2017-04-01 17:13:19 +0800

    修改session

--- Swift3Basics/Cache/Session.swift
+++ Swift3Basics/Cache/Session.swift
@@ -14,11 +14,6 @@ open class Session: LocalStorage {
     
     public static let shareSession = Session()
     
-    open var tick: Int {
-        let result = integer(forKey: "TICK")
-        setObject(((result + 1) % Int.max) as AnyObject?, forKey: "TICK")
-        return result
-    }
     
     /* deviceId */
     open var deviceId: String {
@@ -34,50 +29,5 @@ open class Session: LocalStorage {
         }
         set { setObject(newValue as AnyObject?, forKey: "UDID") }
     }
-    
-    /** uid */
-    open var uid: Int {
-        get { return integer(forKey: "UID") }
-        set { setObject(newValue as AnyObject?, forKey: "UID") }
-    }
-    
-    /** authType */
-    open var authType: String {
-        get { return string(forKey: "AUTH_TYPE", defaultValue: "") }
-        set { setObject(newValue as AnyObject?, forKey: "AUTH_TYPE") }
-    }
-    
-    /** token */
-    open var token: String {
-        get { return string(forKey: "TOKEN", defaultValue: "") }
-        set { setObject(newValue as AnyObject?, forKey: "TOKEN") }
-    }
-    
-    /** imei */
-    open var imei: String {
-        get { return string(forKey: "IMEI", defaultValue: "") }
-        set { setObject(newValue as AnyObject?, forKey: "IMEI") }
-    }
-    
-    /** Location */
-    open var location: CLLocation {
-        get {
-            let latitude = double(forKey: "LATITUDE")
-            let longitude = double(forKey: "LONGITUDE")
-            return CLLocation(latitude: latitude, longitude: longitude)
-        }
-        set {
-            begin()
-            setObject(newValue.coordinate.latitude as AnyObject?, forKey: "LATITUDE")
-            setObject(newValue.coordinate.longitude as AnyObject?, forKey: "LONGITUDE")
-        }
-    }
-    
-    /** city */
-    open var city: String! {
-        get { return string(forKey: "CITY", defaultValue: "") }
-        set { setObject(newValue as AnyObject?, forKey: "CITY") }
-    }
-    
 }
 
